b466d6ab0c363901ed786680189e4013d53ba44e
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-27 13:22:16 +0530

    add dynamic programming

---
+++ dynamic-programming/tabulation/fibonacc-number.ts
@@ -0,0 +1,10 @@
+function fibonacci(n: number): number {
+  if (n <= 2) {
+    return 1;
+  }
+  const fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
